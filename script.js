4ebe76bd76a33fd94c94d9aecc7564259a7bfc9a
BhavnaChaudhary <33368486+BhavnaChaudhary@users.noreply.github.com>
2025-03-18 12:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ nextButton.addEventListener("click", () => {
     currentActive = circles.length;
   }
   updateUi();
-})
+});
 
 prevButton.addEventListener("click", () => {
   currentActive--;
@@ -21,7 +21,7 @@ prevButton.addEventListener("click", () => {
     currentActive = 1;
   }
   updateUi();
-})
+});
 
 function updateUi() {
   circles.forEach((circle, idx) => {
@@ -29,16 +29,17 @@ function updateUi() {
     idx < currentActive
       ? circle.classList.add("active")
       : circle.classList.remove("active");
-  })
+  });
 
   lines.forEach((line, idx) => {
     idx < currentActive - 1
       ? line.classList.add("active")
       : line.classList.remove("active");
-  })
+  });
   prevButton.disabled = currentActive === 1;
   nextButton.disabled = currentActive === circles.length;
 }
+
 
 
 
